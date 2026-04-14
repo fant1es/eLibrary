d70fbf9b597a11ec057de4bf2e6b64eae897c18b
fant1es <genri0206@gmail.com>
2026-04-14 18:33:43 +0300

    feat(admin-ui): implement file dialogs for book and cover selection
    added QFileDialog filters for images (.jpg, .png) and e-books (.pdf, .epub)

--- windows/window_classes.py
+++ windows/window_classes.py
@@ -1,10 +1,37 @@
 from windows import addBookWidget
 from PyQt6 import QtWidgets, QtCore, QtGui
+from PyQt6.QtWidgets import QFileDialog
 
 
 class AddBookWin(QtWidgets.QWidget, addBookWidget.Ui_addBookWidget):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
         self.setWindowFlags(QtCore.Qt.WindowType.WindowCloseButtonHint | QtCore.Qt.WindowType.WindowTitleHint)
+
+        self.review_cover_path_btn.clicked.connect(self.select_cover_path)
+        self.review_book_path_btn.clicked.connect(self.select_book_path)
+        self.cancel_btn.clicked.connect(self.cancel)
+
+    def select_cover_path(self):
+        cover_path, _ = QFileDialog.getOpenFileName(
+            None,
+            "Выберите обложку книги",
+            "",
+            "Images (*.png *.jpg *.jpeg);;All Files (*)"
+        )
+        if cover_path:
+            self.cover_path_edit.setText(cover_path)
+
+    def select_book_path(self):
+        book_path, _ = QFileDialog.getOpenFileName(
+            None,
+            "Выберите файл книги",
+            "",
+            "E-books (*.pdf *.epub *.fb2);;Text Files (*.txt);;All Files (*)"
+        )
+        if book_path:
+            self.book_path_edit.setText(book_path)
+
+    def cancel(self):
+        self.hide()
